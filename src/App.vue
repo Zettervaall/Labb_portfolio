4b5b5980d15f8eaaf954fe43148dc0c9f5c3c4d0
Zettervaall <moa.zettervall@gmail.com>
2025-02-05 00:33:33 +0100

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,20 @@
-<script setup></script>
+<template>
+    <div>
+        <h1>hej moa</h1>
+    </div>
+</template>
+
+<script>
+    let moaVariabel = 'hej Moa';
+
+    console.log(moaVariabel);
+
+    export default {};
+</script>
+
+<!-- <script setup></script>
 
 <template></template>
 
 <style scoped></style>
+ -->
